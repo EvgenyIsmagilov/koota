f947418468e9e0d5aa4a35fe485c9d3691716890
Evgeny Ismagilov <54188204+EvgenyIsmagilov@users.noreply.github.com>
2020-02-03 13:03:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 from distutils.core import setup
+from setuptools import setup, find_packages
+
+def requirements():
+    """Build the requirements list for this project"""
+    requirements_list = []
+
+    with open('requirements.txt') as requirements:
+        for install in requirements:
+            requirements_list.append(install.strip())
+
+    return requirements_list
+
+
+packages = find_packages(exclude=['tests*'])
+requirements = requirements()
+
 
 import pandas as pd # а оно тут надо?
 import numpy as np # а оно тут надо?
@@ -7,8 +23,10 @@ import sys # а оно тут надо?
 setup(name='koota',
       version='0.1',
       description='Library for data analysis and EDA',
+      keywords='python eda data analysis cleaning',
       author='Evgeny Ismagilov',
       author_email='evgenyismagilov@gmail.com',
       url='https://www.python.org/sigs/distutils-sig/',
-      packages=['distutils', 'distutils.command'],
+      packages=packages,
+      install_requires=requirements
      )
