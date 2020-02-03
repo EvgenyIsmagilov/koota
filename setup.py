41b67c432c46db319bcb1eb37119df0882705651
Evgeny Ismagilov <54188204+EvgenyIsmagilov@users.noreply.github.com>
2020-02-03 12:56:11 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+import pandas as pd # а оно тут надо?
+import numpy as np # а оно тут надо?
+import sys # а оно тут надо?
+    
+setup(name='foo',
+      version='1.0',
+      py_modules=['foo'],
+      )
