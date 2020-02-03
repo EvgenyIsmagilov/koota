137203e0ec5f5e27e3aae6619fb10274675c782a
Evgeny Ismagilov <54188204+EvgenyIsmagilov@users.noreply.github.com>
2020-02-03 13:00:05 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from distutils.core import setup
 import pandas as pd # а оно тут надо?
 import numpy as np # а оно тут надо?
 import sys # а оно тут надо?
-    
-setup(name='foo',
-      version='1.0',
-      py_modules=['foo'],
-      )
+
+setup(name='koota',
+      version='0.1',
+      description='Library for data analysis and EDA',
+      author='Evgeny Ismagilov',
+      author_email='evgenyismagilov@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
