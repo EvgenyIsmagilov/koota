4018359cdb02fa52d72f64ab03ca05e60a55999a
Evgeny Ismagilov <54188204+EvgenyIsmagilov@users.noreply.github.com>
2020-02-11 10:21:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,12 @@
-from distutils.core import setup
 from setuptools import setup, find_packages
 
-def requirements():
-    """Build the requirements list for this project"""
-    requirements_list = []
-
-    with open('requirements.txt') as requirements:
-        for install in requirements:
-            requirements_list.append(install.strip())
-
-    return requirements_list
-
-
-
-requirements = requirements()
-
-PACKAGE = ""
-NAME = "koota"
-DESCRIPTION = "Library for data analysis and EDA"
-KEYWORDS = "python eda data analysis cleaning"
-AUTHOR = "Evgeny Ismagilov"
-AUTHOR_EMAIL = "evgenyismagilov@gmail.com"
-URL = "https://github.com/EvgenyIsmagilov/koota"
-VERSION = 0.0.1 #__import__(PACKAGE).__version__
-# = ['mod1', 'pkg.mod2']
-packages = ['koota'] #find_packages()
-
-setup(name=NAME,
-      version=VERSION,
-      description=DESCRIPTION,
-      keywords=KEYWORDS,
-      long_description=read("README.md"),
-      author=AUTHOR,
-      author_email=AUTHOR_EMAIL,
-      url=URL,
-      packages=packages,
-      install_requires=requirements,
-      py_modules = PY_MODULES
-      #, download_url=''
-      #, license=''
-     )
+setup(name='koota',
+    version='0.0.1',
+    description='beta library',
+    author='Jenya',
+    url='https://github.com/EvgenyIsmagilov/koota',
+    packages=['koota'],
+    install_requires=[
+        'unidecode',
+        'pandas'
+    ]
