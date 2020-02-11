aad25d2154f3b41e469304f01f7a7d44542a074d
Evgeny Ismagilov <54188204+EvgenyIsmagilov@users.noreply.github.com>
2020-02-11 19:03:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,20 @@ setup(name='koota',
     description='beta library',
     author='Jenya',
     url='https://github.com/EvgenyIsmagilov/koota',
-    packages=['koota']
+    packages=['koota'],
+    # important
+    license='MIT', 
+    download_url = 'https://github.com/EvgenyIsmagilov/koota/archive/master.zip',
+    keywords = ['EDA', 'DATA', 'CLEANING'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Data Analysts',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+          ]
       #,install_requires=['unidecode']
      )
