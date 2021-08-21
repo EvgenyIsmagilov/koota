c450d94831466fa50b3ae7e2fb15c4d7874582ce
Evgeny Ismagilov <54188204+EvgenyIsmagilov@users.noreply.github.com>
2021-08-21 15:04:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def requirements():
 requirements = requirements()
 
 setup(name='koota',
-    version='0.0.1',
+    version='0.0.2',
     description='Library for data analysis and EDA',
     author='Evgeny Ismagilov',
     author_email="evgenyismagilov@gmail.com",
