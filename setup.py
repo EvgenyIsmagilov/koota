b3ecfa25ef0f37a15afcf888bb20fa6da2a914a0
Evgeny Ismagilov <evgenyismagilov@gmail.com>
2022-02-04 14:23:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = requirements()
 setup(name='koota',
     version='0.0.2',
     description='Library for data analysis and EDA',
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
     # long_description=open('README.md').read(),
     author='Evgeny Ismagilov',
     author_email="evgenyismagilov@gmail.com",
@@ -28,7 +28,7 @@ setup(name='koota',
     keywords = ['EDA', 'DATA', 'CLEANING', 'analysis', 'koota'],
     classifiers=[
         'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        # 'Intended Audience :: Data Analysts',      # Define that your audience are developers
+        'Intended Audience :: Data Analysts',      # Define that your audience are developers
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',   # Again, pick a license
         'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
